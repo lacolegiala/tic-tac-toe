2e3d83a6ce231d564e1e155315d6cee32f1f1546
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2022-03-31 09:58:20 +0300

    Create an empty grid

--- src/ticTacToe.ts
+++ src/ticTacToe.ts
@@ -1,21 +1,5 @@
-type Player = {
-  mark: 'x' | 'o',
-  spaces: number[]
+const grid = Array.from(Array(24).fill(''))
+
+export const isItARow = () => {
+  
 }
-
-const grid = Array.from(Array(24).keys())
-
-const rows: number[][] = [
-  [0, 1, 2, 3, 4],
-  [5, 6, 7, 8, 9],
-  [10, 11, 12, 13, 14],
-  [15, 16, 17, 18, 19],
-  [20, 21, 22, 23, 24],
-  [0, 5, 10, 15, 20],
-  [1, 6, 11, 16, 21],
-  [2, 7, 12, 17, 22],
-  [3, 8, 13, 18, 23],
-  [4, 9, 14, 19, 24],
-  [0, 6, 12, 18, 24],
-  [4, 8, 12, 16, 20]
-]
