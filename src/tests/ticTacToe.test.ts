60ed096bad45723fa06c2e71b3b072c5431677b5
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2022-04-02 19:52:39 +0300

    Create a find winner function

--- src/tests/ticTacToe.test.ts
+++ src/tests/ticTacToe.test.ts
@@ -1,4 +1,4 @@
-import { nextMove } from "../ticTacToe"
+import { findWinnerHorizontal, nextMove } from "../ticTacToe"
 
 const grid = Array.from(Array(24).fill(''))
 
@@ -11,5 +11,17 @@ describe('Next move function ', () => {
   test('does not change the squares that it is not supposed to', () => {
     nextMove(grid, 'x', 7)
     expect(grid[6]).toBe('')
+  })
+})
+
+describe('Find winner function', () => {
+  test('returns x if x has a row', () => {
+    nextMove(grid, 'x', 0)
+    nextMove(grid, 'x', 1)
+    nextMove(grid, 'x', 2)
+    nextMove(grid, 'x', 3)
+    nextMove(grid, 'x', 4)
+    console.log(grid)
+    expect(findWinnerHorizontal(grid)).toBe('x')
   })
 })
--- src/ticTacToe.ts
+++ src/ticTacToe.ts
@@ -1,3 +1,20 @@
 export const nextMove = (grid: string[], player: string, square: number) => {
   grid[square] = player
 }
+
+export const findWinnerHorizontal = (grid: string[]) => {
+  let winner = 'tie'
+  for (let i = 0; i < grid.length; i++) {
+    if (i % 5 === 0) {
+      if (grid[i] !== '') {
+        for (let j = i + 1; j < i + 5; j++) {
+          if (grid[j] !== grid[i]) {
+            break;
+          }
+        }
+        winner = grid[i]
+      }
+    }
+  }
+  return winner 
+}
