ded41f1bbd9966fe5939e31799faa11ae67b96ad
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2022-04-04 20:29:13 +0300

    Add more cases to find winner function and test

--- src/tests/ticTacToe.test.ts
+++ src/tests/ticTacToe.test.ts
@@ -1,4 +1,4 @@
-import { findWinnerHorizontal, nextMove } from "../ticTacToe"
+import { findWinner, nextMove } from "../ticTacToe"
 
 describe('Next move function ', () => {
   const grid = Array.from(Array(24).fill(''))
@@ -21,7 +21,7 @@ describe('Find winner function', () => {
     nextMove(grid, 'x', 2)
     nextMove(grid, 'x', 3)
     nextMove(grid, 'x', 4)
-    expect(findWinnerHorizontal(grid)).toBe('x')
+    expect(findWinner(grid)).toBe('x')
   })
   
   test('returns o if o has a row', () => {
@@ -31,7 +31,47 @@ describe('Find winner function', () => {
     nextMove(grid, 'o', 7)
     nextMove(grid, 'o', 8)
     nextMove(grid, 'o', 9)
-    expect(findWinnerHorizontal(grid)).toBe('o')
+    expect(findWinner(grid)).toBe('o')
+  })
+  
+  test('returns x if x has a column', () => {
+    const grid = Array.from(Array(24).fill(''))
+    nextMove(grid, 'x', 0)
+    nextMove(grid, 'x', 5)
+    nextMove(grid, 'x', 10)
+    nextMove(grid, 'x', 15)
+    nextMove(grid, 'x', 20)
+    expect(findWinner(grid)).toBe('x')
+  })
+  
+  test('returns o if o has a column', () => {
+    const grid = Array.from(Array(24).fill(''))
+    nextMove(grid, 'o', 0)
+    nextMove(grid, 'o', 5)
+    nextMove(grid, 'o', 10)
+    nextMove(grid, 'o', 15)
+    nextMove(grid, 'o', 20)
+    expect(findWinner(grid)).toBe('o')
+  })
+
+  test('returns x if x has a diagonal row', () => {
+    const grid = Array.from(Array(24).fill(''))
+    nextMove(grid, 'x', 0)
+    nextMove(grid, 'x', 6)
+    nextMove(grid, 'x', 12)
+    nextMove(grid, 'x', 18)
+    nextMove(grid, 'x', 24)
+    expect(findWinner(grid)).toBe('x')
+  })
+
+  test('returns o if o has a diagonal row', () => {
+    const grid = Array.from(Array(24).fill(''))
+    nextMove(grid, 'o', 4)
+    nextMove(grid, 'o', 8)
+    nextMove(grid, 'o', 12)
+    nextMove(grid, 'o', 16)
+    nextMove(grid, 'o', 20)
+    expect(findWinner(grid)).toBe('o')
   })
 
   test('returns tie if there are no rows', () => {
@@ -41,6 +81,6 @@ describe('Find winner function', () => {
     nextMove(grid, 'o', 7)
     nextMove(grid, 'x', 8)
     nextMove(grid, 'o', 9)
-    expect(findWinnerHorizontal(grid)).toBe('tie')
+    expect(findWinner(grid)).toBe('tie')
   })
 })
--- src/ticTacToe.ts
+++ src/ticTacToe.ts
@@ -2,13 +2,30 @@ export const nextMove = (grid: string[], player: string, square: number) => {
   grid[square] = player
 }
 
-export const findWinnerHorizontal = (grid: string[]) => {
+export const findWinner = (grid: string[]) => {
   let winner
   while (!winner) {
     for (let i = 0; i < grid.length; i++) {
-      if (i % 5 === 0 && grid[i] !== '') {
-        if ([grid[i], grid[i + 1], grid[i + 2], grid[i + 3], grid[i + 4]].every((value, i, arr) => value === arr[0])) {
-          return grid[i]
+      if (grid[i] !== '') {
+        if (i % 5 === 0) {
+          if ([grid[i], grid[i + 1], grid[i + 2], grid[i + 3], grid[i + 4]].every((value, i, arr) => value === arr[0])) {
+            return grid[i]
+          }
+        }
+        if (i < 5) {
+          if ([grid[i], grid[i + 5], grid[i + 10], grid[i + 15], grid[i + 20]].every((value, i, arr) => value === arr[0])) {
+            return grid[i]
+          }
+        }
+        if (i === 0) {
+          if ([grid[i], grid[i + 6], grid[i + 12], grid[i + 18], grid[i + 24]].every((value, i, arr) => value === arr[0])) {
+            return grid[i]
+          }
+        }
+        if (i === 4) {
+          if ([grid[i], grid[i + 4], grid[i + 8], grid[i + 12], grid[i + 16]].every((value, i, arr) => value === arr[0])) {
+            return grid[i]
+          }
         }
       }
     }
